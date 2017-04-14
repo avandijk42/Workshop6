f0d91de9fd1116c34a044dc28b971d1e51ea4cf9
Alan Van Dijk <hyperstellarsofa@yahoo.com>
2017-04-13 20:36:22 -0400

    static

--- server/src/server.js
+++ server/src/server.js
@@ -7,24 +7,11 @@ var bodyParser = require('body-parser');
 // Creates an Express server.
 var app = express();
 app.use(bodyParser.text());
+// You run the server from `server`, so `../client/build` is `server/../client/build`.
+// '..' means "go up one directory", so this translates into `client/build`!
+app.use(express.static('../client/build'));
 // Defines what happens when it receives the `GET /` request
-app.get('/', function (req, res) {
-  res.send('Hello World!');
-});
 
-// Handle POST /reverse [data]
-app.post('/reverse', function (req, res) {
-  // How do we get the input text?
-  // How do we send the output text?
-  // Check that req.body is a string.
-  if (typeof(req.body) === 'string') {
-    var reversed = reverseString(req.body);
-    res.send(reversed);
-  } else {
-    // POST did not contain a string. Send an error code back!
-    res.status(400).end()
-  }
-});
 
 // Starts the server on port 3000!
 app.listen(3000, function () {
